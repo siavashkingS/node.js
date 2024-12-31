098df44495ac5f5c256fcea8a445081f59742a80
siavash <siavashshafiee13@gmail.com>
2024-12-31 18:55:35 +0330

    Changing the read.js file to show a simple two page site

--- read.js
+++ read.js
@@ -1,7 +1,15 @@
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
+
 http.createServer(function (req, res) {
-  fs.readFile('simple-html.html', function(err, data) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
