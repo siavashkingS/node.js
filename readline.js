8068e2d90da905b0a65c4f017f685222812fd663
siavash <siavashshafiee13@gmail.com>
2025-03-11 18:13:50 +0330

    just some typing error i fixed

--- readline.js
+++ readline.js
@@ -4,11 +4,11 @@ var rl=readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question('How do you feel? ',(anwser)=>{
-    if (anwser=='good'){
+rl.question('How do you feel? ',(answer)=>{
+    if (answer=='good'){
         console.log('Im happy to hear that');
     }
-    else if(anwser=='bad'){
+    else if(answer=='bad'){
         console.log('I hope you get good soon');
     }
     rl.close();
