c58917ee9bf876d72857e02ea0b0662308f58d79
siavash <siavashshafiee13@gmail.com>
2025-04-06 18:53:34 +0330

    connecting to database using '.env', test and practice some sql Commands and queries

---
+++ node_modules/nodejs/node_modules/nodejs/connection.js
@@ -0,0 +1,16 @@
+const mysql=require('mysql');
+const connection=mysql.createConnection({
+    host:process.env.MYSQL_HOST,
+    port:process.env.MYSQL_PORT,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE
+});
+connection.connect(err=>{
+    if(err){
+        console.log('something went wrong',err.message);
+        return;
+    }
+    console.log('database connected');
+})
+module.exports=connection;
---
+++ node_modules/nodejs/node_modules/nodejs/node_modules/nodejs/node_modules/nodejs/node_modules/nodejs/node_modules/nodejs/node_modules/nodejs/node_modules/nodejs/node_modules/nodejs/README.md
@@ -0,0 +1,1 @@
+this is just me learning node.js
